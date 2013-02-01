b662c6d3bf0b12d03f98329d766ce76b5358652a
David.Hernando <david.hernando@2manylanguages.com>
2013-02-01 22:36:54 +0100

    Adding ruby on Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Website::Application.config.secret_token = '628447282514be8d9df1224cb8a57ded66e98a61e73ca4889249ee2b73d8bd9730195b254303b82543f178993d0da9a63293e406586e2c851546e050dff7d30b'
