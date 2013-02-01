68e843711b7b0e4d6074de61ca4e3505baee1aa0
David.Hernando <david.hernando@2manylanguages.com>
2013-02-02 00:36:14 +0100

    * Removing unit tests structure * using MySQL instead of SqLite

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Website::Application.config.secret_token = '628447282514be8d9df1224cb8a57ded66e98a61e73ca4889249ee2b73d8bd9730195b254303b82543f178993d0da9a63293e406586e2c851546e050dff7d30b'
+Website::Application.config.secret_token = 'c81989f074b78a4f9b890d4e9c498ec7867ff793049736d586c98be46a4625230b76a934d642688a25a172b234c96e28b7cdd1374af2642956dc07da671460dd'
